05802a74460552a125f86d7afe0be71714c38e6a
Andrey Geonya <manufacturer.software@gmail.com>
2011-08-24 17:01:18 +0300

    sample fixed

--- example/index.php
+++ example/index.php
@@ -4,7 +4,7 @@ require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'classes' . DIRE
 
 // Init OAuth 2.0 proxy
 $oauth2Proxy = new Oauth2Proxy(
-	'2446676',	// client id
+	'2446676', // client id
 	'oawLNhZCTYRjEJ72ZdET',	// client secret
 	'https://api.vkontakte.ru/oauth/access_token', // access token url
 	'http://api.vkontakte.ru/oauth/authorize', // dialog url
